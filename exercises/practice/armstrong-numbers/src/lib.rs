be824b5071fd9c5692bceac669c674f3062490ab
fuliangliang <fuliangliang@oppo.com>
2022-05-06 20:52:52 +0800

    add armstrong-numbers solution

--- exercises/practice/armstrong-numbers/src/lib.rs
+++ exercises/practice/armstrong-numbers/src/lib.rs
@@ -1,3 +1,29 @@
+fn digit_num(num: u32) -> u32 {
+    let mut p = 1;
+    let mut tmp_num = num;
+    loop {
+        tmp_num /= 10;
+        if tmp_num == 0 {
+            break;
+        }
+        p += 1;
+    }
+    p
+}
+
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let p = digit_num(num);
+    let mut sum = 0;
+    let mut num_mut = num;
+
+    loop {
+        let m = num_mut % 10;
+        sum += m.pow(p);
+        num_mut /= 10;
+        if num_mut == 0 {
+            break;
+        }
+    }
+
+    sum == num
 }
