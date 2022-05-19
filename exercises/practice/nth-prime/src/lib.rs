0157b31091059baacb8b5970f0345ece943ef552
fuliangliang <fuliangliang@oppo.com>
2022-05-19 15:44:08 +0800

    add nth-prime solution

--- exercises/practice/nth-prime/src/lib.rs
+++ exercises/practice/nth-prime/src/lib.rs
@@ -1,3 +1,29 @@
+pub fn is_prime(n: u32) -> bool {
+    if n == 2 {
+        return true;
+    }
+
+    let m = (n as f64).sqrt() as u32 + 1;
+
+    for i in 2..m {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut count = 0;
+
+    for i in 2.. {
+        if is_prime(i) {
+            count += 1;
+        }
+
+        if count == n + 1 {
+            return i;
+        }
+    }
+    return 0;
 }
